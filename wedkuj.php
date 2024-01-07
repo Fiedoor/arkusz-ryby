f453c9606e69f6ac59e0cf8e4e159bb53886faad
Fiedor <stachfie@wp.pl>
2024-01-07 15:21:07 +0100

    Html done

---
+++ wedkuj.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <main>
+        <div id="lewy1">
+            <h3>Ryby zamieszkujące rzeki</h3>
+            <ol>
+                <?php
+                //s1
+                ?>
+            </ol>
+        </div>
+        <div id="lewy2">
+            <h3>Ryby drapieżne naszych wód</h3>
+            <table>
+                <tr>
+                    <th>L.p.</th>
+                    <th>Gatunek</th>
+                    <th>Występowanie</th>
+                </tr>
+                <?php
+                //s2
+                ?>
+            </table>
+        </div>
+        <div id="prawy">
+            <img src="ryba1.jpg" alt="Sum">
+            <br>
+            <a href="kwerendy.txt" download>Pobierz kwerendy</a>
+        </div>
+    </main>
+    <footer>
+        <p>Stronę wykonał: NR ARKUSZA</p>
+    </footer>
+</body>
+
+</html>
